ff5b882ca1a0b870f46c56caf07a92ab8eb120dd
Michael Aquilina <michaelaquilina@gmail.com>
2017-07-03 19:01:37 +0100

    Add is_not_substring_of

--- src/assertions.zsh
+++ src/assertions.zsh
@@ -15,6 +15,18 @@ function _zunit_assert_is_substring_of() {
 }
 
 ###
+# Assert one string is not a substring if another
+###
+function _zunit_assert_is_not_substring_of() {
+  local value=$1 comparison=$2
+
+  [[ "$comparison" != *"$value"* ]] && return 0
+
+  echo "'$value' is a substring of '$comparison'"
+  exit 1
+}
+
+###
 # Assert that two integers are equal
 ###
 function _zunit_assert_equals() {
--- tests/assertions/is_substring_of.zsh
+++ tests/assertions/is_substring_of.zsh
@@ -1,13 +1,36 @@
 #!/usr/bin/env zunit
 
-@test 'substring matches' {
-  assert 'world' is_substring_of 'hello world'
-  assert 'hello' is_substring_of 'hello world'
-  assert 'lo wo' is_substring_of 'hello world'
-  assert '*' is_substring_of '-*-'
+@test 'substring match' {
+  run assert 'lo wo' is_substring_of 'hello world'
+
+  assert "$output" is_empty
+  assert $state equals 0
+}
+
+@test 'substring suffix match' {
+  run assert 'world' is_substring_of 'hello world'
+
+  assert "$output" is_empty
+  assert $state equals 0
+}
+
+@test 'substring prefix match' {
+  run assert 'hello' is_substring_of 'hello world'
+
+  assert "$output" is_empty
+  assert $state equals 0
 }
 
 @test 'whole word matches' {
-  assert 'red blue green' is_substring_of 'red blue green'
-  assert '*---*' is_substring_of '*---*'
+  run assert 'red blue green' is_substring_of 'red blue green'
+
+  assert "$output" is_empty
+  assert $state equals 0
+}
+
+@test 'substring match failure' {
+  run assert 'foo' is_substring_of 'elephants'
+
+  assert "$output" same_as "'foo' is not a substring of 'elephants'"
+  assert $state equals 1
 }
